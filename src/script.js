3f669580b70cbb57d6dce3ec847ab759537df352
Sakharuk Viktar <natgeogr93@gmail.com>
2020-12-14 23:06:17 +0300

    feat: table draw

--- src/script.js
+++ src/script.js
@@ -1,8 +1,24 @@
 import './styles/style.css';
 
-function test(a) {
-  console.log('всё работает', a);
-}
-test(100);
+const timeToggle = document.querySelector('.time-toggle--elem');
+const timeToggleText = document.querySelector('.time-toggle--text');
+const countToggle = document.querySelector('.count-toggle--elem');
+const countToggleText = document.querySelector('.count-toggle--text');
 
-document.querySelector('body').classList.add('neddl');
+function changeToggleText() {
+  timeToggleText.textContent = (timeToggle.hasAttribute('data-day')) ? 'за последний день' : 'за весь период пандемии';
+  countToggleText.textContent = (countToggle.hasAttribute('data-100')) ? 'на 100 тыс. населения' : 'всего';
+}
+
+function toggleElement({ target }) {
+  const isTimeToggleElement = target.classList.contains('time-toggle--elem');
+  if (isTimeToggleElement) {
+    target.toggleAttribute('data-day');
+  } else {
+    target.toggleAttribute('data-100');
+  }
+  changeToggleText();
+}
+
+timeToggle.addEventListener('click', toggleElement);
+countToggle.addEventListener('click', toggleElement);
